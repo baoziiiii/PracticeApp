3c192f4f160574ecc7e932542ff627f313ac9884
John Bao <452651705@qq.com>
2018-01-28 00:36:22 +0800

    Initial commit

---
+++ 3.1 Button and Dialog/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){ //为View设置监听器
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(MainActivity.this).setTitle("系统提示") //弹出提示框
+                        .setMessage("真的要开始吗？")
+                        .setPositiveButton("确定", new DialogInterface.OnClickListener() { //为提示框设置监听器
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Log.i("桌面台球", "进入游戏");//控制台输出
+                            }
+                        }).setNegativeButton("退出",new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("桌面台球", "退出游戏");
+                        finish(); //退出应用
+                    }
+                }).show();
+            }});
+    }
+}
---
+++ 3.2 GridLayout/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img[]=new ImageView[12];
+    private int[] imgPath=new int[]{R.mipmap.img01,R.mipmap.img02,R.mipmap.img03,R.mipmap.img04,
+            R.mipmap.img05,R.mipmap.img06,R.mipmap.img07,R.mipmap.img08,R.mipmap.img09,
+            R.mipmap.img10,R.mipmap.img11,R.mipmap.img12};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);    //指定布局文件
+        GridLayout grid=(GridLayout)findViewById(R.id.gridlayout);
+        for (int i = 0; i <imgPath.length ; i++) {
+            img[i]=new ImageView(this);
+            img[i].setImageResource(imgPath[i]);
+            img[i].setPadding(2,2,2,2);
+
+            ViewGroup.LayoutParams params=new ViewGroup.LayoutParams(100,50); //创建一个关于布局参数的对象
+            img[i].setLayoutParams(params);  //设置布局宽度和高度
+            grid.addView(img[i]);
+        }
+    }
+}
