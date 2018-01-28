23cc8a33193bd3271e488145dab7d53c94853b52
John Bao <452651705@qq.com>
2018-01-28 15:05:49 +0800

    add clear button

--- 3.1 Button and Dialog/src/main/java/com/example/MainActivity.java
+++ 3.1 Button and Dialog/src/main/java/com/example/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
